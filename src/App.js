02e47f6995c9a8e1788a1f9343a16432c891d26b
Tito <tito.parra4@@hotmail.com>
2019-04-12 22:07:16 +0200

    CreateComponents

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
 import React, { Component } from 'react';
 import Header from './componentes/Header';
 import Formulario from './componentes/Formulario';
+import Error from './componentes/Error';
 
 class App extends Component {
+	state = {
+		error: '',
+		consulta: {},
+		resultado: {}
+	};
+
+	componentDidUpdate() {
+		this.consultarApi();
+	}
+
+	componentDidMount() {
+		this.setState({
+			error: false
+		});
+	}
+
+	consultarApi = () => {
+		const { ciudad, pais } = this.state.consulta;
+		if (!ciudad || !pais) return null;
+
+		const appId = '3e114dfec79e505e09d7c5ab1f4eeaa3';
+		let url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+		fetch(url)
+			.then((respuesta) => {
+				return respuesta.json();
+			})
+			.then((datos) => {
+				this.setState({
+					resultado: datos
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+	datosConsulta = (respuesta) => {
+		if (respuesta.ciudad === '' || respuesta.pais === '') {
+			this.setState({
+				error: true
+			});
+		} else {
+			this.setState({
+				consulta: respuesta
+			});
+		}
+	};
+
 	render() {
+		const error = this.state.error;
+
+		let resultado;
+
+		if (error) {
+			resultado = <Error mensaje="The two fields are obligatory" />;
+		}
+
 		return (
 			<div className="app">
 				<Header titulo="Weather React" />
-				<Formulario />
+				<Formulario datosConsulta={this.datosConsulta} />
+				{resultado}
 			</div>
 		);
 	}
--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
 
 class Fomulario extends Component {
+	ciudadRef = React.createRef();
+	paisRef = React.createRef();
+
+	buscarClima = (e) => {
+		e.preventDefault();
+
+		const respuesta = {
+			ciudad: this.ciudadRef.current.value,
+			pais: this.paisRef.current.value
+		};
+		//console.log(respuesta);
+
+		this.props.datosConsulta(respuesta);
+	};
+
 	render() {
 		return (
 			<div className="contenedor-form">
 				<div className="container">
 					<div className="row">
-						<form>
+						<form onSubmit={this.buscarClima}>
 							<div className="input-field col s12 m8 l4 offset-m2">
-								<input id="ciudad" type="text" />
-								<label for="ciudad">City:</label>
+								<input id="ciudad" ref={this.ciudadRef} type="text" />
+								<label htmlFor="ciudad">City:</label>
+							</div>
+							<div className="input-field col s12 m8 l4 offset-m2">
+								<select ref={this.paisRef}>
+									<option value="" defaultValue>
+										Select a country
+									</option>
+									<option value="PL">Poland</option>
+									<option value="CO">Colombia</option>
+									<option value="US">USA</option>
+								</select>
+								<label htmlFor="pais">Country:</label>
+							</div>
+							<div className="input-field col s12 m8 l4 offset-2 buscador">
+								<input
+									type="submit"
+									className="waves-effect waves-light btn-large yellow accent-4"
+									value="Search....."
+								/>
 							</div>
 						</form>
 					</div>
